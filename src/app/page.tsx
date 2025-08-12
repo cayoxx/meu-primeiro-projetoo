585d13ab33dbc94479608a7cd67cf46ff056a7fe
cayoxx <caiomercedesamaral@gmail.com>
2025-08-12 12:28:56 -0300

    PRIMEIRO COMMIT

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className="bg-black h-[100dvh]">
+    <h1 className="text-6xl text-purple-600 bg-black">Caio Amaral</h1>
+    <div className ="flex justify-center items center h-[400px] w-[100vw] bg-red-500">
+      <div className = "h-[100px] w-[100px] bg-black"></div>
+
+    </div>
+    </section>
+  );
+}
